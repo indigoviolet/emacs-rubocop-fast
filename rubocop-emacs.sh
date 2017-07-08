ca975eb69bfd11d1019fe079c6bb560292b6438c
Venky Iyer <indigoviolet@users.noreply.github.com>
2017-07-07 23:41:30 -0700

    Update rubocop-emacs.sh

--- rubocop-emacs.sh
+++ rubocop-emacs.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Rubocop has an annoying behavior with --auto-correct --stdin where it combines the offense report and the corrected output
+# https://github.com/bbatsov/rubocop/issues/2502#issuecomment-252184708
+#
+# Fix by splitting into STDOUT and STDERR
+
 rubocop $* | perl -0777 -ne '/(.*?)^=+$\\n(.*)/ms; print STDERR $1; print STDOUT $2;'
